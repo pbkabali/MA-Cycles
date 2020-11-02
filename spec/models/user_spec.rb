e34cf98d5b35ff1d9c45e0585111f9e4cd45601b
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-11-02 06:12:02 +0300

    Generate users scaffold and configure database

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'cannot have comments' do   #
+    expect(2).to eq(2)
+  end
+end
